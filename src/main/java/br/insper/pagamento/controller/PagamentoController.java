dc5e3f328dab6da2bb3d99ab097a03422af32d21
Eduardo Santana <ezambomsantana@gmail.com>
2026-08-12 09:50:31 -0300

    exercici

--- src/main/java/br/insper/pagamento/controller/PagamentoController.java
+++ src/main/java/br/insper/pagamento/controller/PagamentoController.java
@@ -1,6 +1,7 @@
 package br.insper.pagamento.controller;
 
 import br.insper.pagamento.dto.PagamentoDto;
+import br.insper.pagamento.dto.RespostaPagamentoDto;
 import br.insper.pagamento.entity.Pagamento;
 import br.insper.pagamento.exception.ValidacaoPagamentoException;
 import br.insper.pagamento.service.PagamentoService;
@@ -47,20 +48,12 @@ public class PagamentoController {
 
 	@PostMapping("/{id}/processar")
 	public ResponseEntity<?> processar(@PathVariable Long id) {
-		try {
-			boolean sucesso = pagamentoService.processar(id);
-			Map<String, Object> resposta = new HashMap<>();
-			resposta.put("sucesso", sucesso);
-			resposta.put("mensagem", sucesso ? "Pagamento processado com sucesso" : "Erro ao processar pagamento");
-			return ResponseEntity.ok(resposta);
-		} catch (ValidacaoPagamentoException e) {
-			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(erroResponse(e.getMessage()));
-		}
+		boolean sucesso = pagamentoService.processar(id);
+		RespostaPagamentoDto resposta = new RespostaPagamentoDto(
+			sucesso,
+			sucesso ? "Pagamento processado com sucesso" : "Erro ao processar pagamento"
+		);
+		return ResponseEntity.ok(resposta);
 	}
 
-	private Map<String, String> erroResponse(String mensagem) {
-		Map<String, String> erro = new HashMap<>();
-		erro.put("erro", mensagem);
-		return erro;
-	}
 }
